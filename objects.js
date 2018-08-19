4074e5b7c5a62d0cbd790c0f5515e9147ce90233
saipaul@hotmail.com <saipaul@hotmail.com>
2018-08-19 20:50:15 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,10 +2,10 @@ var playlist = {adele:'hello'}
 console.log(playlist)
 function updatePlaylist (playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
-  console.log(playlist)
   return playlist
 }
 function removeFromPlaylist (playlist, artistName) {
   delete playlist.artistName;
+  console.log(playlist)
   return playlist
 }
